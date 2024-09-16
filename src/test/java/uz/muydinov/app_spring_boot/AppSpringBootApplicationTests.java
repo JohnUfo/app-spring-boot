88a5dbb9385045a6e13b5d1692f85170105aff41
johnUfo <muydinovumidjon17@gmail.com>
2024-09-16 22:05:04 +0500

    first commit

---
+++ src/test/java/uz/muydinov/app_spring_boot/AppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.muydinov.app_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
